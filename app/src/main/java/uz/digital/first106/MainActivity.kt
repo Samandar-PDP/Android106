a2fb9c62e0ce5c83bb9172640ffac78ce9060faa
samandar-mee <samandarcoderdev@gmail.com>
2023-02-08 17:28:34 +0500

    Activity main ui changed

--- app/src/main/java/uz/digital/first106/MainActivity.kt
+++ app/src/main/java/uz/digital/first106/MainActivity.kt
@@ -3,7 +3,9 @@ package uz.digital.first106
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.button.MaterialButton
@@ -12,7 +14,11 @@ import com.google.android.material.textfield.TextInputEditText
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.mobile_banking)
+        supportActionBar?.hide()
+        val spinner: Spinner = findViewById(R.id.spinner)
+        val list = listOf("UZ","RU","EN","US")
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+        spinner.adapter = arrayAdapter
     }
 }
